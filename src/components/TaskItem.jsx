e0bc0d9c5a6fc0f230e8139c1ef87f19e7577fed
Sérgio Oliveira <sergiowallace11@hotmail.com>
2025-02-27 16:53:36 +0000

    improving styles task container.

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -15,9 +15,9 @@ import { FaCheck } from 'react-icons/fa';
 function TaskItem({ task, id, onToggle, onDelete, onEdit }) {
   const [edit, setEdit] = useState(false);
   const [newTask, setNewTask] = useState('');
-
+  
   return (
-    <div className="bg-gray-400 flex items-center gap-5 border border-gray-300/90 rounded-md p-3 w-full">
+    <div className="bg-gray-400 flex items-center justify-between gap-5 border border-gray-300/90 rounded-md p-3 w-[600px]">
       {/* checkbox container */}
       <Checkbox
         checked={task.completed}
@@ -30,7 +30,7 @@ function TaskItem({ task, id, onToggle, onDelete, onEdit }) {
         <Input value={newTask} onChange={(e) => setNewTask(e.target.value)} />
       ) : (
         <p
-          className={`text-white text-left text-xl ${
+          className={`text-white text-left text-xl${
             task.completed ? 'line-through text-white/40' : ''
           }`}
         >
@@ -38,29 +38,32 @@ function TaskItem({ task, id, onToggle, onDelete, onEdit }) {
         </p>
       )}
 
-      {/* trash button */}
-      <Button
-        type="button"
-        variant="destructive"
-        onClick={() => onDelete(id)}
-        aria-label="Delete task"
-      >
-        <FaTrashAlt />
-      </Button>
+      {/* button container */}
+      <div className="flex items-center justify-center gap-3">
+        <Button
+          type="button"
+          onClick={() => onDelete(id)}
+          aria-label="Delete task"
+          className="w-8 h-8"
+        >
+          <FaTrashAlt />
+        </Button>
 
-      <Button onClick={() => setEdit(true)}>
-        <FaEdit />
-      </Button>
+        <Button onClick={() => setEdit(true)} className="w-8 h-8">
+          <FaEdit />
+        </Button>
 
-      <Button
-        type="button"
-        onClick={() => {
-          onEdit({ id: task.id, newTask });
-          setEdit(false);
-        }}
-      >
-        <FaCheck />
-      </Button>
+        <Button
+          type="button"
+          onClick={() => {
+            onEdit({ id: task.id, newTask });
+            setEdit(false);
+          }}
+          className="w-8 h-8"
+        >
+          <FaCheck />
+        </Button>
+      </div>
     </div>
   );
 }
